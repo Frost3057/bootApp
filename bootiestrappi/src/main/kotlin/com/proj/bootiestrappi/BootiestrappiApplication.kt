8ddf7d635cbd010d712b502424147e0be028a050
Frost3057 <amanbajpai5660@gmail.com>
2025-05-17 23:16:52 +0530

    initial commit

---
+++ bootiestrappi/src/main/kotlin/com/proj/bootiestrappi/BootiestrappiApplication.kt
@@ -0,0 +1,11 @@
+package com.proj.bootiestrappi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootiestrappiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootiestrappiApplication>(*args)
+}
---
+++ bootiestrappi/src/test/kotlin/com/proj/bootiestrappi/BootiestrappiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.proj.bootiestrappi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootiestrappiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
